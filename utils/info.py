2aef8af54a1608ab18922df38ed257ffddba7da9
AreBhaai <160462350+AreBhaai@users.noreply.github.com>
2025-02-22 01:06:37 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -18,13 +18,13 @@
 
 import os
 
-API_ID       = int(os.environ.get("API_ID", ""))
-API_HASH     = os.environ.get("API_HASH", "")
-BOT_TOKEN    = os.environ.get("BOT_TOKEN", "")
+API_ID       = int(os.environ.get("API_ID", "18674011"))
+API_HASH     = os.environ.get("API_HASH", "38d3664512757d8830601169eff5a1de")
+BOT_TOKEN    = os.environ.get("BOT_TOKEN", "7945022794:AAEdwZt7jCowhJ-ZO1QVQhRTG1OJV5kZ6Ag")
 SESSION      = os.environ.get("SESSION", "")
-TIME         = int(os.environ.get("TIME", 10))
-CHATS        = [int(cht) for cht in os.environ.get("CHATS", "").split()]
+TIME         = int(os.environ.get("TIME", 2))
+CHATS        = [int(cht) for cht in os.environ.get("CHATS", "-1002251825993 -1002274439678 -1002262472330 -1001643696419 -1002130385569").split()]
 WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
 BLACK_LIST   = [int(blk) for blk in os.environ.get("BLACK_LIST", "").split()]
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://C:C@cluster0.danekjk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 PORT         = os.environ.get("PORT", "8080")
